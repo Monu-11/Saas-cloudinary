a88e85e0755fdb984ca079f78ffd9d99beea8442
Monu-11 <monukumarsinghmonu@gmail.com>
2024-09-25 10:33:20 +0530

    Made some change

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,10 @@ const nextConfig = {
       },
     ],
   },
+  env: {
+    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME:
+      process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  },
 };
 
 export default nextConfig;
