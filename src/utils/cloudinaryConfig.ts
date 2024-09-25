60a63f122d0a5a6f7c9da65775e64e0b9f4b3839
Monu-11 <monukumarsinghmonu@gmail.com>
2024-09-25 11:38:51 +0530

    Make some change for sometime

--- src/utils/cloudinaryConfig.ts
+++ src/utils/cloudinaryConfig.ts
@@ -1,14 +1,14 @@
 import { v2 as cloudinary } from 'cloudinary';
 
 cloudinary.config({
-  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  cloud_name: 'dmq9ig2eh',
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export function checkCloudinaryCredentials() {
   if (
-    !process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME ||
+    !'dmq9ig2eh' ||
     !process.env.CLOUDINARY_API_KEY ||
     !process.env.CLOUDINARY_API_SECRET
   ) {
